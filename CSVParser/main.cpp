54af570794e0bff1fd100196703028b569342c67
johnkonstantin <danilajjj@mail.ru>
2023-01-21 12:41:29 +0700

    Add CSVParser

---
+++ CSVParser/main.cpp
@@ -0,0 +1,15 @@
+#include "CSVParser.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ifstream a("input.csv");
+
+	CSVParser<string, double> parser(a, 0);
+	parser.setDelimiters('\"', ',', '\n');
+
+	for (tuple<string, double> el: parser)
+		cout << el << endl;
+
+	return 0;
+}
